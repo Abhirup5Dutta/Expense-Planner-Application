2e616e9f272b33a413c1fe5f70f8681dbd853b00
Abhirup <abhirupdutta04acse@gmail.com>
2020-12-30 15:54:57 +0530

    Making a Expense Planner Flutter App as a part of practicing Flutter app development from an Udemy Course by Instructor Maximillian Schwarzmuller

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,40 @@
+import 'package:ExpensePlanner/models/transaction.dart';
+import 'package:flutter/material.dart';
+import './transaction_item.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final Function deleteTx;
+
+  TransactionList(this.transactions, this.deleteTx);
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            );
+          })
+        : ListView.builder(
+            itemBuilder: (ctx, index) {
+              return TransactionItem(
+                  transaction: transactions[index], deleteTx: deleteTx);
+            },
+            itemCount: transactions.length,
+          );
+  }
+}
